1c8c563db9e5a0ac625d4b39ea9d68daff290632
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-06 15:39:31 +0900

    [feat] styled button 컴포넌트 추가

--- app/src/main/java/com/uzun/pseudosendy/presentation/_const/UIConst.kt
+++ app/src/main/java/com/uzun/pseudosendy/presentation/_const/UIConst.kt
@@ -3,6 +3,7 @@ package com.uzun.pseudosendy.presentation._const
 import androidx.compose.ui.unit.dp
 
 object UIConst {
+    // spacing
     val SPACE_XXS = 4.dp
     val SPACE_XS = 8.dp
     val SPACE_S = 12.dp
@@ -10,10 +11,15 @@ object UIConst {
     val SPACE_L = 20.dp
     val SPACE_XL = 24.dp
     val SPACE_XXL = 32.dp
+
+    // button
     val BUTTON_WIDTH_LARGE = 360.dp
     val BUTTON_WIDTH_NORMAL = 280.dp
     val BUTTON_WIDTH_SMALL = 232.dp
     val BUTTON_HEIGHT_LARGE = 30.dp
     val BUTTON_HEIGHT_NORMAL = 27.dp
     val BUTTON_HEIGHT_SMALL = 24.dp
+    val BUTTON_RADIUS_LARGE = 30.dp
+    val BUTTON_RADIUS_NORMAL = 27.dp
+    val BUTTON_RADIUS_SMALL = 24.dp
 }
---
+++ app/src/main/java/com/uzun/pseudosendy/presentation/ui/common/StyleButtons.kt
@@ -0,0 +1,116 @@
+package com.uzun.pseudosendy.presentation.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.uzun.pseudosendy.presentation._const.UIConst.BUTTON_RADIUS_LARGE
+import com.uzun.pseudosendy.presentation._const.UIConst.BUTTON_RADIUS_NORMAL
+import com.uzun.pseudosendy.presentation._const.UIConst.BUTTON_RADIUS_SMALL
+import com.uzun.pseudosendy.presentation._const.UIConst.BUTTON_WIDTH_LARGE
+import com.uzun.pseudosendy.presentation._const.UIConst.BUTTON_WIDTH_NORMAL
+import com.uzun.pseudosendy.presentation._const.UIConst.BUTTON_WIDTH_SMALL
+import com.uzun.pseudosendy.presentation._const.UIConst.SPACE_M
+import com.uzun.pseudosendy.ui.theme.DayBlueBase
+import com.uzun.pseudosendy.ui.theme.DayGrayscale400
+import com.uzun.pseudosendy.ui.theme.White
+
+enum class ButtonSize(val width: Dp, val radius: Dp, val padding: Dp) {
+    NORMAL(BUTTON_WIDTH_NORMAL, BUTTON_RADIUS_NORMAL, SPACE_M),
+    SMALL(BUTTON_WIDTH_SMALL, BUTTON_RADIUS_SMALL, 11.dp),
+    LARGE(BUTTON_WIDTH_LARGE, BUTTON_RADIUS_LARGE, SPACE_M),
+}
+
+@Composable
+fun BaseRoundedButton(
+    onClick: () -> Unit,
+    type: ButtonSize,
+    colors: ButtonColors,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(type.radius),
+        colors = colors,
+        modifier = Modifier
+            .padding(vertical = type.padding)
+            .width(type.width),
+        content = content
+    )
+}
+
+@Composable
+fun BaseSquareButton(
+    onClick: () -> Unit,
+    type: ButtonSize,
+    colors: ButtonColors,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(0.dp),
+        colors = colors,
+        modifier = Modifier
+            .padding(vertical = type.padding)
+            .width(type.width),
+        content = content
+    )
+}
+
+@Composable
+fun RoundedPrimaryButton(
+    onClick: () -> Unit = {},
+    type: ButtonSize = ButtonSize.NORMAL,
+    content: @Composable RowScope.() -> Unit = {},
+) {
+    BaseRoundedButton(
+        onClick = onClick,
+        type = type,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = DayBlueBase,
+            contentColor = White,
+            disabledBackgroundColor = DayGrayscale400,
+            disabledContentColor = White,
+        ),
+        content = content
+    )
+}
+
+@Composable
+fun SquarePrimaryButton(
+    onClick: () -> Unit = {},
+    type: ButtonSize = ButtonSize.NORMAL,
+    content: @Composable RowScope.() -> Unit = {},
+) {
+    BaseSquareButton(
+        onClick = onClick,
+        type = type,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = DayBlueBase,
+            contentColor = White,
+            disabledBackgroundColor = DayGrayscale400,
+            disabledContentColor = White,
+        ),
+        content = content
+    )
+}
+
+@Preview
+@Composable
+fun PreviewButtons() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(White)
+    ) {
+        RoundedPrimaryButton() { Text("Text for Test") }
+        SquarePrimaryButton() { Text("Text for Test") }
+    }
+}
