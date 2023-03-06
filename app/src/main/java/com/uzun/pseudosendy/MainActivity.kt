aade094578775888fda764c80b59dcfe226845f2
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-06 09:29:27 +0900

    [chore] 프로젝트 초기 설정 추가

---
+++ app/src/main/java/com/uzun/pseudosendy/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.uzun.pseudosendy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.uzun.pseudosendy.ui.theme.PseudoSendyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PseudoSendyTheme {
+                OrderFormScreen()
+            }
+        }
+    }
+}
