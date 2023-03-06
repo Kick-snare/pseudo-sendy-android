ef0855cfc400d5ea3d89487e2e16be2921f8a329
YooJun Hong <46425142+Kick-snare@users.noreply.github.com>
2023-03-06 14:18:31 +0900

    Issue #4 디자인 리소스 및 상수 추가 (#8)
    * [design] color 상수 추가 (in CamelCase)
    
    * [chore] NotoSansKR 폰트 추가
    
    * [design] NotoSansKR FontFamilly 추가
    
    * [design] custom typography 추가
    
    * [feat] Custom Typo을 위한 CompositionLocalProvider 적용
    
    * [design] LocalProvider Theme에 적용
    
    * [chore] 벡터 이미지 리소스 (xml)로 추가
    
    * [design] UIConst 화면 상수 값 설정
    
    * [fix] custom typo object 추가

--- app/src/main/java/com/uzun/pseudosendy/MainActivity.kt
+++ app/src/main/java/com/uzun/pseudosendy/MainActivity.kt
@@ -8,9 +8,10 @@ import com.uzun.pseudosendy.ui.theme.PseudoSendyTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             PseudoSendyTheme {
-                OrderFormScreen()
+                // OrderFormScreen()
             }
         }
     }
--- app/src/main/java/com/uzun/pseudosendy/ui/theme/Type.kt
+++ app/src/main/java/com/uzun/pseudosendy/ui/theme/Type.kt
@@ -1,28 +1,116 @@
 package com.uzun.pseudosendy.ui.theme
 
-import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.uzun.pseudosendy.R
+import javax.annotation.concurrent.Immutable
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+
+val NotoSansKR = FontFamily(
+    Font(R.font.notosanskr_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.notosanskr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosanskr_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosanskr_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.notosanskr_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.notosanskr_thin, FontWeight.Thin, FontStyle.Normal),
+)
+
+@Immutable
+data class SendyTypography(
+    val XXXLBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp
+    ),
+    val XXXL : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+    val XXLBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    ),
+    val XXL : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    ),
+    val XLBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    val XL : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    val LargeBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    val Large : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    val MediumBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    val Medium : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+    val NormalBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    val Normal : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    ),
+    val SmallBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    val Small : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    val XSBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 13.sp
+    ),
+    val XS : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp
+    ),
+    val XXSBold : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp
+    ),
+    val XXS : TextStyle = TextStyle(
+        fontFamily = NotoSansKR,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
-    )
-    */
+    ),
 )
+
+val sendyTypography = SendyTypography()
