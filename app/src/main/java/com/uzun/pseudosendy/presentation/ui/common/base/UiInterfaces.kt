7f5b43370ece293f841cce6daa0e63c75fab4e84
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-07 09:48:42 +0900

    [feat] 베이스 뷰 모델 구현

---
+++ app/src/main/java/com/uzun/pseudosendy/presentation/ui/common/base/UiInterfaces.kt
@@ -0,0 +1,16 @@
+package com.uzun.pseudosendy.presentation.ui.common.base
+
+/**
+ * UiEvent is the user actions.
+ */
+interface UiEvent
+
+/**
+ * UiSideEffect is the side effects like error messages which we want to show only once.
+ */
+interface UiSideEffect
+
+/**
+ * UiState is current state of views.
+ */
+interface UiState
