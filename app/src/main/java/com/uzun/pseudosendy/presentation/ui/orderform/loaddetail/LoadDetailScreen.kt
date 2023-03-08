f4c9ef6ec80bd52b9d5a2ff8f06d736311f36da4
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-08 17:49:13 +0900

    [feat] Vehicle Selection 화면에 dropdown 적용

--- app/src/main/java/com/uzun/pseudosendy/presentation/ui/orderform/loaddetail/LoadDetailScreen.kt
+++ app/src/main/java/com/uzun/pseudosendy/presentation/ui/orderform/loaddetail/LoadDetailScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
@@ -58,7 +57,6 @@ fun LazyListScope.bottomGuideText() = item {
     )
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 fun LazyListScope.textAreaField(
     text: String = "",
     onValueChanged: (String) -> Unit = {},
