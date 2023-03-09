5c55f5ee1be76255a7c66745cc86d620cef5e9df
uzun <hyjhyj0901@pusan.ac.kr>
2023-03-09 15:21:32 +0900

    [fix] DatePicker, TimePicker 기존 값을 받아 표시할 수 있게 변경

--- app/src/main/java/com/uzun/pseudosendy/presentation/ui/orderform/datetime/TimePicker.kt
+++ app/src/main/java/com/uzun/pseudosendy/presentation/ui/orderform/datetime/TimePicker.kt
@@ -8,24 +8,31 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun TimePicker(
+    value: String,
     onValueChange: (String) -> Unit = {},
     content: @Composable RowScope.(String, () -> Unit) -> Unit,
 ) {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    val time = if (value.isNotBlank()) LocalTime.parse(value, formatter) else LocalTime.now()
     val mTime = remember { mutableStateOf("") }
-    val time = LocalTime.now()
 
     val dialog = TimePickerDialog(
         LocalContext.current,
         { _, mHour: Int, mMinute: Int ->
             onValueChange(LocalTime.of(mHour, mMinute).toString())
-            mTime.value = if(mHour > 12) "오후 ${mHour-12} 시 $mMinute 분" else "오전 $mHour 시 $mMinute 분"
         },
         time.hour,
         time.minute,
-        false,
+        true,
     )
-    Row(){ content(mTime.value, dialog::show) }
+
+    mTime.value =
+        if(time.hour > 12) "오후 ${time.hour-12} 시 ${time.minute} 분"
+        else "오전 ${time.hour} 시 ${time.minute} 분"
+
+    Row(){ content(if(value.isNotBlank()) mTime.value else "", dialog::show) }
 }
